f6126db8e446794f0ef83b394ab2d7a216b63047
jisooyoon <yoonmallang@gmail.com>
2023-10-24 16:03:26 +0900

    ✨ feat: 컴퓨터 숫자 3개 선택

--- src/App.js
+++ src/App.js
@@ -2,4 +2,29 @@ class App {
   async play() {}
 }
 
+class Computer {
+  #numbers;
+
+  makeThreeNums() {
+    const candidate = Array(9)
+      .fill()
+      .map((v, i) => i + 1);
+
+    const shuffle = [];
+
+    while (shuffle.length < 3) {
+      const random = Math.floor(Math.random() * candidate.length);
+      const spliceArray = candidate.splice(random, 1);
+      const value = spliceArray[0];
+      shuffle.push(value);
+    }
+
+    this.#numbers = shuffle;
+  }
+
+  getThreeNums() {
+    return this.#numbers;
+  }
+}
+
 export default App;
