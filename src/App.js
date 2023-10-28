792f8eb718873cd209da99dada8d1d772e3d5f6a
jisooyoon <yoonmallang@gmail.com>
2023-10-28 13:47:05 +0900

    ✨ feat: 게임 플레이

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  computer = new Computer();
+  player = new Player();
+  manager = new Manager();
+
+  async play() {
+    let keepPlaying = true;
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.computer.makeThreeNums();
+
+    try {
+      while (keepPlaying) {
+        const playerInput = await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ");
+
+        if (this.manager.checkPlayerThreeNumsWrong(playerInput)) {
+          throw new Error("[ERROR] 잘못된 값을 입력하였습니다.");
+        }
+        this.player.guessThreeNums(playerInput);
+
+        this.manager.compareNums(this.player.getThreeNums(), this.computer.getThreeNums());
+        MissionUtils.Console.print(this.manager.printResult());
+
+        if (this.manager.printResult().split("\n")[0] === "3스트라이크") {
+          const restartInput = await MissionUtils.Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+          if (!this.manager.checkPlayerRestartNum(restartInput)) {
+            throw new Error("[ERROR] 1 또는 2 이외의 값을 입력하였습니다.");
+          }
+
+          if (this.manager.checkPlayerWantsRestart(restartInput)) {
+            this.computer.makeThreeNums();
+            continue;
+          }
+
+          keepPlaying = false;
+          MissionUtils.Console.print("게임종료");
+        }
+      }
+    } catch (error) {
+      MissionUtils.Console.print("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
+      throw error;
+    }
+  }
 }
 
 class Computer {
@@ -67,8 +111,7 @@ class Manager {
       return "낫싱";
     } else if (this.#strikeNum === 3) {
       return `3스트라이크
-3개의 숫자를 모두 맞히셨습니다! 게임 종료
-게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
+3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
     } else {
       return `${this.#ballNum ? this.#ballNum + "볼" : ""} ${this.#strikeNum ? this.#strikeNum + "스트라이크" : ""}`.trim();
     }
